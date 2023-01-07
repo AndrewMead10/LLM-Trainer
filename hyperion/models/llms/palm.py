489bda5485750aea6871f87e476532fdfb6c1a07
andrew mead <ammead@umass.edu>
2023-01-07 02:19:18 -0500

    Regular implimentation for reference against flash attn

--- hyperion/models/llms/palm.py
+++ hyperion/models/llms/palm.py
@@ -7,6 +7,39 @@ from typing import Optional
 
 class TransformerBlock(nn.Module):
     def __init__(self, cfg):
+        self.mha = nn.MultiheadAttention(
+            embed_dim=cfg.d_model,
+            num_heads=cfg.n_heads,
+            dropout=cfg.attn_dropout,
+            bias=cfg.bias,
+            batch_first=True,
+            add_bias_kv=False,
+
+        )
+
+        self.ffn = nn.Sequential(
+            nn.Linear(cfg.d_model, cfg.mlp_ratio * cfg.d_model),
+            nn.GELU(),
+            nn.Linear(cfg.mlp_ratio * cfg.d_model, cfg.d_model),
+        )
+
+        self.ln1 = nn.LayerNorm(cfg.d_model)
+        self.ln2 = nn.LayerNorm(cfg.d_model)
+        self.dropout1 = nn.Dropout(cfg.dropout)
+        self.dropout2 = nn.Dropout(cfg.dropout)
+
+    def forward(self, x: torch.Tensor, key_padding_mask: Optional[torch.ByteTensor] = None):
+        a = self.ln1(x)
+        b, _ = self.mha(a, a, a, key_padding_mask)
+        x = x + self.dropout1(b)
+        c = self.ln2(x)
+        d = self.ffn(c)
+        x = x + self.dropout2(d)
+        return x
+
+
+class FusedTransformerBlock(nn.Module):
+    def __init__(self, cfg):
         self.mha = FlashMHA(
             embed_dim=cfg.d_model,
             num_heads=cfg.n_heads,
@@ -46,9 +79,14 @@ class Transformer(nn.Module):
         self.embed = nn.Embedding(cfg.vocab_size, cfg.d_model)
         self.pos_enc = nn.Embedding(cfg.max_len, cfg.d_model)
 
-        self.blocks = nn.ModuleList(
-            [TransformerBlock(cfg) for _ in range(cfg.n_layers)]
-        )
+        if cfg.fused:
+            self.blocks = nn.ModuleList(
+                [FusedTransformerBlock(cfg) for _ in range(cfg.n_layers)]
+            )
+        else:
+            self.blocks = nn.ModuleList(
+                [TransformerBlock(cfg) for _ in range(cfg.n_layers)]
+            )
 
         self.ln = nn.LayerNorm(cfg.d_model)
         self.head = nn.Linear(cfg.d_model, cfg.vocab_size)
